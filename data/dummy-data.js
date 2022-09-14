a87491c20940ba5c5cc6a651b1cba9a17c823204
Robert Yarborough <admin@robert-yarborough.com>
2022-09-14 13:58:01 -0400

    dummy-data edited and MealDetailScreen component added with child components obfuscated into components directory

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -4,14 +4,14 @@ import Meal from '../models/meal';
 export const CATEGORIES = [
   new Category('c1', 'Italian', '#f5428d'),
   new Category('c2', 'Quick & Easy', '#f54242'),
-  new Category('c3', 'Hamburgers', '#f5a442'),
+  new Category('c3', 'All-American', '#f5a442'),
   new Category('c4', 'German', '#f5d142'),
   new Category('c5', 'Light & Lovely', '#368dff'),
-  new Category('c6', 'Exotic', '#41d95d'),
+  new Category('c6', 'African', '#41d95d'),
   new Category('c7', 'Breakfast', '#9eecff'),
-  new Category('c8', 'Asian', '#b9ffb0'),
+  new Category('c8', 'Soul Food', '#b9ffb0'),
   new Category('c9', 'French', '#ffc7ff'),
-  new Category('c10', 'Summer', '#47fced')
+  new Category('c10', 'Indian', '#47fced')
 ];
 
 export const MEALS = [
