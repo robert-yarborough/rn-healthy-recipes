2883e7dc41dc9afd6e1a38056498e41e3df3416d
Robert Yarborough <admin@robert-yarborough.com>
2022-09-14 13:16:41 -0400

    INITIAL COMMIT FILES

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,17 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+
+function renderCategoryItem(itemData){
+  return <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+}
+
+function CategoriesScreen(){
+  return <FlatList
+              keyExtractor={(item) => item.id}
+              data={CATEGORIES}
+              renderItem={renderCategoryItem}
+              numColumns={2}/>
+}
+export default CategoriesScreen;
